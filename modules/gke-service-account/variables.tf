c785398b2c0e55e7fa725ae1e6494928d77c3643
Harikrishna Talapaneni <hartalap@publicisgroupe.net>
2024-01-17 18:08:16 +0000

    final push

--- modules/gke-service-account/variables.tf
+++ modules/gke-service-account/variables.tf
@@ -1,8 +1,4 @@
-# ---------------------------------------------------------------------------------------------------------------------
 # REQUIRED MODULE PARAMETERS
-# These parameters must be supplied when consuming this module.
-# ---------------------------------------------------------------------------------------------------------------------
-
 variable "project" {
   description = "The name of the GCP Project where all resources will be launched."
   type        = string
@@ -13,10 +9,7 @@ variable "name" {
   type        = string
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL MODULE PARAMETERS
-# These parameters have reasonable defaults.
-# ---------------------------------------------------------------------------------------------------------------------
 
 variable "description" {
   description = "The description of the custom service account."
